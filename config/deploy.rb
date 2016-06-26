59f01513b1080973214fd6011ac997e153c6acb7
Khalid Alsaadi <kh.alsaady@gmail.com>
2016-06-26 20:54:13 +0530

    Update deployment config

--- config/deploy.rb
+++ config/deploy.rb
@@ -90,15 +90,15 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rvm_type, :user                     
 set :rvm_ruby_version, '2.2.3@api_examples'
 
-namespace :deploy do
+# namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+#   after :restart, :clear_cache do
+#     on roles(:web), in: :groups, limit: 3, wait: 10 do
+#       # Here we can do anything such as:
+#       # within release_path do
+#       #   execute :rake, 'cache:clear'
+#       # end
+#     end
+#   end
 
 end
